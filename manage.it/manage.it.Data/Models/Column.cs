3a238c80b24e6793dd61926fd0075a68265255e4
gabrieln <niemiecgabriel@gmail.com>
2016-10-26 21:50:05 +0200

    #2 - Create back-end application architecture

---
+++ manage.it/manage.it.Data/Models/Column.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace manage.it.Data.Models
+{
+    public class Column
+    {
+    }
+}
---
+++ manage.it/manage.it.Data/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("manage.it.Data")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d189097-aeeb-4485-96d0-5e23f6929e6a")]
